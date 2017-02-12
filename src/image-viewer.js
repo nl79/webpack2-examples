9c23f18cb39a30a9adb6e7e2079667d097acbb9d
Nazar Lesiv <nazarlesiv@Nazars-MBP.home>
2017-02-11 19:22:46 -0500

    CSS processing output and Image processing.

--- src/image-viewer.js
+++ src/image-viewer.js
@@ -1,7 +1,13 @@
+import big from '../assets/big.jpeg';
+import small from '../assets/small.jpeg';
 import '../styles/image-viewer.css';
 
 const image = document.createElement('img');
-image.src = 'http://lorempixel.com/400/400';
+image.src = small;
+
+const bigImage = document.createElement('img');
+bigImage.src = big;
 
 console.log('document',document.body);
 document.body.appendChild(image);
+document.body.appendChild(bigImage);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const config = {
   // Entry point into the application.
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    // Other modules like the 'url-loader' will prepend the value of the
+    // 'publicPath' property to the urls that is generates.
+    // Other loaders will also use this property to generate URLs.
+    publicPath: 'build/'
   },
   module: {
     // Load specific modules based on the filename and extension.
@@ -21,11 +26,32 @@ const config = {
          * therefore the css-loader will first load the data and the style-loader
          * will process it.
          */
-        use: ['style-loader', 'css-loader'],
+        //use: ['style-loader', 'css-loader'],
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
         test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          // Apply the loaders.
+          // If a loader requires specific options, use an Object with the loader and options
+          // properties.
+          {
+            loader: 'url-loader',
+            // If an image is larger than 40000 bites, saved it in a separate file.
+            // If an image is smaller than 40000 bites, save the data as part of the bundle.
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
 
 module.exports = config;
